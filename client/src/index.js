c0cc49155ad95c19f8be0f21b67606732cd5f50c
owen <on-hill@outlook.com>
2023-05-26 20:32:36 -0500

    rename app.js to app.jsx / remove import index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import reactDom, { createRoot } from 'react-dom/client';
-import App from './components/App/App.js';
+import { createRoot } from 'react-dom/client';
+import App from './components/App/App';
 import './index.css';
 
 const domNode = document.getElementById('root');
