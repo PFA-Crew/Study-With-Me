503fdf99ea86736f2739fce73d909639cb804555
Kasualkid12 <kasu@kasu-it.dev>
2023-05-27 10:33:47 -0500

    modularize notes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,12 @@
 const User = require('../models/userModel');
-const Note = require('../models/noteModel');
 
 const userController = {};
 
 userController.createUser = async (req, res, next) => {
   try {
-    // Deconstruct data from request body
     const createdUser = await User.create(req.body);
-    // username, password, layouts data
-    // Process obtained data
     console.log(createdUser);
     res.locals.userID = createdUser;
-    // Persist data to res.locals
-    // Invoke next middleware
     return next();
   } catch (error) {
     // Handle error
@@ -29,26 +23,8 @@ userController.verifyUser = async (req, res, next) => {
     if (dbCheck && pwCheck) {
       console.log('logged in');
       // res.locals.userID = dbCheck._id;
-      next();
+      return next();
     }
-  } catch (error) {
-    // Handle error
-    return next(error);
-  }
-};
-
-// get users notes
-userController.getUserNotes = async (req, res, next) => {
-  try {
-    const { noteIDs } = req.body.noteIDs;
-    const userNotes = await Promise.all(noteIDs.map(async (id) => {
-      await Note.find({ noteID: id });
-    }));
-    // Process obtained data
-    // Persist data to res.locals
-    res.locals.notes = userNotes;
-    // Invoke next middleware
-    return next();
   } catch (error) {
     // Handle error
     return next(error);
--- server/index.js
+++ server/index.js
@@ -1,29 +1,18 @@
 const express = require('express');
-
-const PORT = 1234;
 // const path = require('path');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const auth = require('./routes/auth');
 
-const app = express();
+const PORT = 1234;
 
-const MONGO_URI = 'mongodb+srv://williaminterlock:acorn@cluster0.xxhf1te.mongodb.net/?retryWrites=true&w=majority';
-mongoose
-  .connect(MONGO_URI, {
-    // options for the connect method to parse the URI
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // sets the name of the DB that our collections are part of
-    dbName: 'Study_With_Me',
-  })
-  .then(() => console.log('Connected to Mongo DB.'))
-  .catch((err) => console.log(err));
+const app = express();
 
 app.use(express.json());
 
 app.use('/auth', auth);
 
 app.get('/', (req, res) => {
+  // figure out what to do with base route
   res.send('howdy howdy');
 });
 
@@ -42,4 +31,6 @@ app.use((err, req, res) => {
   res.status(500).send({ error: err });
 });
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
