ab0373df4d35a9c5c4e1c0d7a41385e9559680e8
Kasualkid12 <kasu@kasu-it.dev>
2023-05-28 03:54:05 -0500

    add clarifying comments to bacckend code

--- server/controllers/notesController.js
+++ server/controllers/notesController.js
@@ -3,19 +3,26 @@ const { Users } = require('../models/userModel');
 
 const notesController = {};
 
+// Note creator middleware
 notesController.createNote = async (req, res, next) => {
   try {
+    // Destructure title, conttent and username from our request body
     const { title, content, username } = req.body;
 
+    // Add a new note object to our notes document
     const createdNote = await Notes.create({ title, content });
+    // Deconstruct the id from the new note
     const { _id } = createdNote;
+    // Deconstruct the notes array from the users document, finding the user that matches the username
     const { notes } = await Users.findOne({ username });
+    // Create a new array passing in the id of our new note and spreading the array of note ids to the new array
     const newNotes = [_id, ...notes];
 
-    //update relevant user
+    // Update relevant user notes array with our new array of notes ids
     await Users.findOneAndUpdate({ username }, { notes: newNotes });
     res.locals.note = createdNote;
 
+    // Move to our next middleware
     return next();
   } catch (err) {
     //Handle error
--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -6,12 +6,13 @@ const userController = require('../controllers/userController');
 // const cookieController = require('../controllers/cookieController');
 // const sessionController = require('../controllers/sessionController');
 
+// Route to handle creating a new user
 router.post('/create', userController.createUser, (req, res) => {
   res.status(201).json(res.locals);
 });
 
+// Route to find and verify a user
 router.post('/login', userController.verifyUser, (req, res) => {
-  // find data in db and return json layouts
   res.status(201).json(res.locals);
 });
 
