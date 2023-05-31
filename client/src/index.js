eba64442276e29c4bcf742a3b53131a4f129891c
Douglas Yao <doug.yao@gmail.com>
2023-05-31 09:20:42 -0700

    swap filenames for MainContainer and App

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,12 @@ import {
 } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
-import MainContainer from './MainContainer.jsx';
+import App from './App.jsx';
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 root.render(
   <BrowserRouter>
-    <MainContainer />
+    <App />
   </BrowserRouter>,
 );
-
