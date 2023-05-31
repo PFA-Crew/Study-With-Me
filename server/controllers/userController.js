3734a1a3f1122ac9f6cfaecb140a5c4a389d16ca
Connor Dillon <connor@codewithconnor.com>
2023-05-30 20:43:15 -0700

    :recycle: Refactor verifyUser middleware - Rewrite verifyUser to be self-documenting: -- Remove verbose comments without sacrificing readability - Code split password check: -- Move bcrypt.matchPassword into private method on User model

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -37,39 +37,23 @@ userController.verifyUser = async (req, res, next) => {
     const { username, password } = req.body;
 
     // Find our user in our mongoDB document based off username
-    const dbCheck = await Users.findOne({ username });
+    const user = await Users.findOne({ username });
 
-    // Check if our user exists, if they dont return to out global error handler
-    /**
-     * TODO: adjust this so if the username or password don't exist they send the error back to the frontend in the same timeframe
-     * ie. if the username is incorrect, it should take the same amount of time as an incorect password so malicious actors
-     *  can't determine that a password is correct by timing the response
-     */
-    if (!dbCheck) {
+    // If user was found, compare the password our user typed in with the hashed password we have stored for the user
+    if (user && (await user.matchPassword(password))) {
+      res.locals.user = {
+        username: user.username,
+        notes: user.notes,
+        duckColor: user.duckColor
+      };
+      return next();
+    } else {
       return next({
-        log: 'bad username', // displays only to server
-        status: 400,
+        log: 'Bad Username', // displays only to server
+        status: 401,
         message: { err: 'wrong login credentials' },
       });
-    }
-
-    // Deconstruct duckColor and notes from our user
-    const { duckColor, notes } = dbCheck;
-    // Compare the password our user typed in with the hashed password we have stored for the user
-    const pwCheck = await bcrypt.compare(password, dbCheck.password);
-
-    // If the passwords match store user data, not including password, in response locals to send to front end
-    if (pwCheck) {
-      res.locals.user = { username, notes, duckColor };
-      return next();
-    }
-
-    // If the passwords dont match send an error to our global error handler
-    return next({
-      log: 'bad password', // displays only to server
-      status: 400,
-      message: { err: 'wrong login credentials' },
-    });
+  }
   } catch (err) {
     // Handle error
     return next({
